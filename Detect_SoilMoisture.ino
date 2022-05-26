e60504eb17d6bd798b21beb798c2192f0657b48a
hussnainshafiul <106338425+hussnainshafiul@users.noreply.github.com>
2022-05-27 01:00:57 +0500

    Update Detect_SoilMoisture.ino

--- Detect_SoilMoisture.ino
+++ Detect_SoilMoisture.ino
@@ -12,3 +12,17 @@ const char *pass =  "shafi1234"; // WiFi Password
 /*For OLED DISPLAY*/
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64  
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); //-1 means OLED do not have a reset pin 
+
+WiFiClient client;
+
+//AidaFruit Credentials
+
+#define MQTT_SERV "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_NAME "hussshafiul" // Adafruit IO Username
+#define MQTT_PASS "aio_DwVh62wuoHlT2v36r04gpu1LYEDQ" // Adafruit IO AIO key
+
+const int Sensor_key = 36;             
+int Sensor_value;   
