9fd577488842c5a2c449526a6c0da9615c252922
hussnainshafiul <106338425+hussnainshafiul@users.noreply.github.com>
2022-05-27 23:47:46 +0500

    Update Detect_SoilMoisture.ino

--- Detect_SoilMoisture.ino
+++ Detect_SoilMoisture.ino
@@ -26,3 +26,19 @@ WiFiClient client;
 
 const int Sensor_pin = 36;             
 int Sensor_value;   
+
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERV, MQTT_PORT, MQTT_NAME, MQTT_PASS);
+Adafruit_MQTT_Publish Sensor_data = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/ESP32_Soil_Moisture_Monitor");  
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");            
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
